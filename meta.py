4b819c4bc634763094a0634145a9417b1934f6cd
melpon <melt@e-ml.jp>
2015-09-25 17:17:09 +0900

    implements C++ version meta. (https://github.com/cpprefjp/site/issues/256)

--- meta.py
+++ meta.py
@@ -11,6 +11,8 @@ from __future__ import unicode_literals
     ... * function[meta id-type]
     ... * std[meta namespace]
     ... * vector[meta class]
+    ... * cpp11deprecated[meta cpp]
+    ... * cpp14removed[meta cpp]
     ...
     ... 本文
     ... '''
@@ -19,7 +21,7 @@ from __future__ import unicode_literals
     <h1>コードをここに書く</h1>
     <p>本文</p>
     >>> md._meta_result
-    {'header': 'vector', 'id-type': 'function', 'namespace': 'std', 'class': 'vector'}
+    {'header': ['vector'], 'id-type': ['function'], 'namespace': ['std'], 'class': ['vector'], 'cpp': ['cpp11deprecated', 'cpp14removed']}
 """
 
 from __future__ import absolute_import
@@ -57,7 +59,9 @@ class MetaPreprocessor(Preprocessor):
             if m:
                 target = m.group('target')
                 name = m.group('name')
-                self._markdown._meta_result[name] = target
+                if name not in self._markdown._meta_result:
+                    self._markdown._meta_result[name] = []
+                self._markdown._meta_result[name].append(target)
             else:
                 new_lines.append(line)
         return new_lines
@@ -69,20 +73,51 @@ class MetaPostprocessor(postprocessors.Postprocessor):
         postprocessors.Postprocessor.__init__(self, md)
         self._markdown = md
 
+    CPP_DIC = {
+        'cpp11': {
+            'class_name': 'cpp11',
+            'title': 'C++11で追加',
+            'text': '(C++11)',
+        },
+        'cpp14': {
+            'class_name': 'cpp14',
+            'title': 'C++14で追加',
+            'text': '(C++14)',
+        },
+        'cpp11deprecated': {
+            'class_name': 'cpp11deprecated text-warning',
+            'title': 'C++11で非推奨',
+            'text': '(C++11で非推奨)',
+        },
+        'cpp14deprecated': {
+            'class_name': 'cpp14deprecated text-warning',
+            'title': 'C++14で非推奨',
+            'text': '(C++14で非推奨)',
+        },
+        'cpp14removed': {
+            'class_name': 'cpp14removed text-danger',
+            'title': 'C++11で削除',
+            'text': '(C++11で削除)',
+        },
+    }
+
     def run(self, text):
         if not hasattr(self._markdown, '_meta_result'):
             return text
 
         meta = self._markdown._meta_result
 
+        if 'cpp' in meta:
+            for name in meta['cpp']:
+                text = text.replace('</h1>', '<span class="cpp {class_name}" title="{title}">{text}</span></h1>'.format(**self.CPP_DIC[name]))
         if 'class' in meta:
-            text = text.replace('<h1>', '<h1><span class="class" title="class {cls}">{cls}::</span>'.format(cls=meta['class']))
+            text = text.replace('<h1>', '<h1><span class="class" title="class {cls}">{cls}::</span>'.format(cls=meta['class'][0]))
         if 'namespace' in meta:
-            text = text.replace('<h1>', '<h1><span class="namespace" title="namespace {ns}">{ns}::</span>'.format(ns=meta['namespace']))
+            text = text.replace('<h1>', '<h1><span class="namespace" title="namespace {ns}">{ns}::</span>'.format(ns=meta['namespace'][0]))
         if 'header' in meta:
-            text = '<div class="header">&lt;{}&gt;</div>'.format(meta['header']) + text
+            text = '<div class="header">&lt;{}&gt;</div>'.format(meta['header'][0]) + text
         if 'id-type' in meta:
-            text = '<div class="identifier-type">{}</div>'.format(meta['id-type']) + text
+            text = '<div class="identifier-type">{}</div>'.format(meta['id-type'][0]) + text
         return text
 
 
