f7115f020b5da9601d3e5f4b56498105b852d8d1
Akira Takahashi <faithandbrave@gmail.com>
2017-04-24 17:55:40 +0900

    コミット後の変換ですぐにregexライブラリが使われ、pip install regexされていない状況でインポートエラーになっていたため、一旦戻し

--- qualified_fenced_code.py
+++ qualified_fenced_code.py
@@ -33,7 +33,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import regex as re
+import re
 
 from markdown.extensions.codehilite import CodeHilite
 from markdown.extensions.codehilite import CodeHiliteExtension
