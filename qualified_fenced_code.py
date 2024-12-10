941d376cacf8e9edb6c2e81c3ab0d25951b968b2
Koichi Murase <myoga.murase@gmail.com>
2024-12-10 22:02:44 +0900

    コード修飾の箇条書きは * であることを要請

--- qualified_fenced_code.py
+++ qualified_fenced_code.py
@@ -41,15 +41,15 @@ from markdown.preprocessors import Preprocessor
 CODE_WRAP = '<pre><code%s>%s</code></pre>'
 LANG_TAG = ' class="%s"'
 
-# qualifier の各行は以下の形式を持つことを要求する。箇条書きまたは番号リストの
-# 項目であり、[meta ...], [mathjax enable ...], [link ...], [color ...],
-# [italic] の何れかの修飾子が含まれていること。インデントレベルは少なくとも ```
-# と同じであること。
-QUALIFIER_LINE_RE_STRING = r'(?P=indent)\s*(?:[-*+]|[0-9]+\.)\s[^\n]*\[(?:meta|mathjax enable|link|color|italic)\b[^\n]*\][^\n]*\n'
+# qualifier の各行は以下の形式を持つことを要求する。"*" による箇条書きの項目で
+# あり、[meta ...], [mathjax enable ...], [link ...], [color ...], [italic] の
+# 何れかの修飾子が含まれていること。インデントレベルは少なくとも閉じ ``` と同じ
+# であること。
+QUALIFIER_LINE_RE_STRING = r'(?P=indent)\s*\*\s[^\n]*\[(?:meta|mathjax enable|link|color|italic)\b[^\n]*\][^\n]*\n'
 
 # 以下の正規表現は qualifier 行の連続を規定する。最初の qualifier が、閉じ ```
-# と同じレベルの箇条書きまたは番号リストの項目でなければそこで中断する。
-QUALIFIERS_RE_STRING = r'(?:(?!(?P=indent)(?:[-*+]|[0-9]+\.)\s)|(?P<qualifies>(?:%s)*))' % QUALIFIER_LINE_RE_STRING
+# と同じレベルの "*" による箇条書きの項目でなければそこで中断する。
+QUALIFIERS_RE_STRING = r'(?:(?!(?P=indent)\*\s)|(?P<qualifies>(?:%s)*))' % QUALIFIER_LINE_RE_STRING
 
 QUALIFIED_FENCED_BLOCK_RE = re.compile(r'(?P<fence>`{3,})[ ]*(?P<lang>[a-zA-Z0-9_+-]*)(?P<lang_meta>.*?)\n(?P<code>.*?)(?<=\n)(?P<indent>[ \t]*)(?P=fence)[ ]*\n' + QUALIFIERS_RE_STRING, re.MULTILINE | re.DOTALL)
 QUALIFY_COMMAND_RE = re.compile(r'\[(.*?)\]')
